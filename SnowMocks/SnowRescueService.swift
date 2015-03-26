251fb14fc1ef050f0892cd01f5766709ce894462
Adam Dąbrowski <adam.dabrowski@me.com>
2015-03-26 16:11:09 +0100

    sander is always send

--- SnowMocks/SnowRescueService.swift
+++ SnowMocks/SnowRescueService.swift
@@ -2,7 +2,14 @@ import Foundation
 
 class SnowRescueService {
 
+    private let municipalService: MunicipalService
+
+    init(weatherForecastService: WeatherForecastService, municipalService: MunicipalService) {
+        self.municipalService = municipalService
+    }
+
     func checkForecastAndRescue() {
+        municipalService.sendSander()
     }
 
 }
--- SnowMocksTest/SnowMocksTest.swift
+++ SnowMocksTest/SnowMocksTest.swift
@@ -2,17 +2,28 @@ import Cocoa
 import XCTest
 
 class SnowMocksTest: XCTestCase {
+    let weatherForecastService = MockWeatherForecastService()
+    let municipalService = MockMunicipalService()
+    var snowRescueService: SnowRescueService!
 
     override func setUp() {
+        self.snowRescueService = SnowRescueService(weatherForecastService: weatherForecastService,
+                municipalService: municipalService)
     }
 
     func test_SendsASanderWhenTemperatureBelow0() {
+        weatherForecastService.temperature = -1
+
+        snowRescueService.checkForecastAndRescue()
+
+        XCTAssertTrue(municipalService.sanderWasSent)
     }
 
     class MockWeatherForecastService: WeatherForecastService {
 
+        var temperature = 0
         func getTemperatureInCelcius() -> Int {
-            return 0
+            return temperature
         }
 
         func getSnowFallHeightInMm() -> Int {
@@ -25,6 +36,7 @@ class SnowMocksTest: XCTestCase {
         private(set) var sanderWasSent = false
 
         func sendSander() {
+            sanderWasSent = true
         }
 
         func sendSnowPlow() -> Result {
